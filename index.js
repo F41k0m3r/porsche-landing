2a42b98a411b5c13823ba34ab014ea0f57f53740
F41k0m3r <105551615+F41k0m3r@users.noreply.github.com>
2023-02-26 21:27:10 +0300

    final

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+const slides = document.querySelectorAll('.slide')
+
+
 document.querySelector('.button').onclick = () => {
     document.querySelector('#block2').scrollIntoView({
         block:'nearest',
         behavior:'smooth'
     })
 }
+slides.forEach((slide) => {
+    slide.addEventListener('click', ()=>{
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+        slide.classList.add('active')
+
+    })
+})
+
